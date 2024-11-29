fe6055ad9424a5420a7dc0f08d786051f7594e00
alpacaive <w6352800@naver.com>
2024-11-30 04:45:36 +0900

    문제점

--- src/test/java/alpacaive/stock/service/StockServiceTest.java
+++ src/test/java/alpacaive/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -37,6 +41,36 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+        // 익스큐터 서비스 사용
+        // 익스큐터 서비스는 비동기로 실행하는 작업을 단순화하여 사용할 수 있게 도와조는 JAVA의 API
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+
+        // CountDownLatch 는 다른 Thread 에서 수행 중인 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - ( 1 * 100 ) = 0
+        assertEquals(0, stock.getQuantity());
+        // 예상 결과가 다르다
+        // 이유 : 레이스 컨디션이 일어났기 때문
+        // 레이스 컨디션이란 둘 이상의 Thread 가 공유 데이터에 엑세스 할 수 있고 동시에 변경을 하려고 할때 발생하는 문제
     }
 
 }
